25d79b1548d6d6a7833651798ddff49204ae360e
GustavoOmai <omai.gustavoh@gmail.com>
2024-06-18 21:06:08 -0300

    Fix | import in local variable

--- src/components/temperature/index.tsx
+++ src/components/temperature/index.tsx
@@ -6,14 +6,7 @@ import arrowDown from "../../assets/arrowDown.svg"
 
 function Temperature() {
 
-    const apiKey: string = import.meta.env.EACT_APP_API_KEY
-
-    console.log(import.meta.env.REACT_APP_API_KEY);
-
-    console.log(import.meta.env.VITE_SOME_KEY)
-
-    console.log(import.meta.env.MODE);
-    
+    const apiKey: string = import.meta.env.VITE_REACT_APP_API_KEY
 
     const fetchTemperature = async () => {
         return await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Paris`)
